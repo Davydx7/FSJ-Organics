59d266549b8aff4732c94220cc2eb23c74e15f62
Davydx7 <oludave0511@gmail.com>
2022-06-18 00:43:53 +0100

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { Outlet } from 'react-router-dom';
@@ -10,7 +11,16 @@ import useTheme from './Stores/themeStore';
 const queryClient = new QueryClient();
 
 function App() {
+  const changeTheme = useTheme((state) => state.changeTheme);
+
   const theme = useTheme((state) => state.theme);
+
+  useLayoutEffect(() => {
+    if (localStorage.getItem('theme') === 'dark') {
+      changeTheme();
+    }
+  }, []);
+
 
   return (
     <QueryClientProvider client={queryClient}>
--- src/Stores/themeStore.ts
+++ src/Stores/themeStore.ts
@@ -4,9 +4,15 @@ import { ThemeStore } from './typings';
 const useTheme = create<ThemeStore>((set) => ({
   theme: 'light',
   changeTheme() {
-    set((state) => (
-      state.theme === 'light' ? { theme: 'dark' } : { theme: 'light' }
-    ));
+    set((state) => {
+      if (state.theme === 'light') {
+        localStorage.setItem('theme', 'dark');
+        return { theme: 'dark' };
+      } else {
+        localStorage.setItem('theme', 'light');
+        return { theme: 'light' };
+      }
+    });
   },
 }));
 
